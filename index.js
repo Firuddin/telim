7eb78a6808f2f54750211af303359e54661ff4ac
Firuddin <Firuddinaliyev888@gmail.com>
2024-03-19 02:07:51 -0700

    add

---
+++ index.js
@@ -0,0 +1,4 @@
+let uzunluq = prompt("zehmet olmasa duzbucaqlinin uzunluqun daxil edin")
+let en = prompt("zehmet olmasa duzbucaqlinin enin daxil edin")
+let common = en*uzunluq
+console.log(common);
