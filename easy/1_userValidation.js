1efbcffba4822b85ce381d4220c5db699920caaf
Firuddin <Firuddinaliyev888@gmail.com>
2024-03-21 14:50:42 -0700

    add

--- easy/1_userValidation.js
+++ easy/1_userValidation.js
@@ -1,5 +1,5 @@
 export const userValidation = (str) => {
-  if (str.length >= 4 && str.length <= 25 && (/[a-zA-Z]/).test(str.slice(0,1))&&(/^\w+$/).test(str)) {
+  if (str.length >= 4 && str.length <= 25 && (/[a-zA-Z]/).test(str.slice(0,1))&&(/^\w+$/).test(str)&& (/[a-zA-Z0-9]/).test(str.slice(-1))) {
     return true;
   }else{
     return false
