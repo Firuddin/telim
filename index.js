d70f4343fd4a929ffe78c19344e9d06a9c8bce49
Firuddin <Firuddinaliyev888@gmail.com>
2024-03-21 05:58:36 -0700

    add

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-let uzunluq = prompt("zehmet olmasa duzbucaqlinin uzunluqun daxil edin")
-let en = prompt("zehmet olmasa duzbucaqlinin enin daxil edin")
-let common = en*uzunluq
-console.log(common);
+let uzunluq = +prompt("zehmet olmasa duzbucaqlinin uzunluqun daxil edin");
+let en = +prompt("zehmet olmasa duzbucaqlinin enin daxil edin");
+/*let common = en*uzunluq
+alert(common);
+
+let common = 2 * (en + uzunluq);
+console.log("perimetr:", common);*/
