13af8856651ed0f3bf3458e18f56ca197f1dfac4
Firuddin <Firuddinaliyev888@gmail.com>
2024-03-24 15:13:18 -0700

    add

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 /*import { FirstElement } from "./folderJs/Uservaliadtion.js";
 console.log(FirstElement(8));
 */
-import { NewElement } from "./folderJs/Uservaliadtion.js";
-console.log(NewElement("salam"));
+/*import { NewElement } from "./folderJs/Uservaliadtion.js";
+console.log(NewElement("salam"));*/
+import { ThreeElement } from "./folderJs/Uservaliadtion.js";
+console.log(ThreeElement("necesen"));
--- folderJs/Uservaliadtion.js
+++ folderJs/Uservaliadtion.js
@@ -9,9 +9,14 @@
 
 */
 //Task2
-export const NewElement = (name)=>{
+/*export const NewElement = (name)=>{
  let hello = name.split("")
  let newWord = hello.reverse()
  let common = newWord.join("")
 return common
+}*/
+
+//Task3
+export const ThreeElement =(param)=>{
+  return param.split("").reverse("").join("")
 }
