5e9e95c3e224f96f0dc1ea8946f76a2259929520
Firuddin <Firuddinaliyev888@gmail.com>
2024-03-25 04:26:15 -0700

    add

--- app.js
+++ app.js
@@ -3,5 +3,7 @@ console.log(FirstElement(8));
 */
 /*import { NewElement } from "./folderJs/Uservaliadtion.js";
 console.log(NewElement("salam"));*/
-import { ThreeElement } from "./folderJs/Uservaliadtion.js";
-console.log(ThreeElement("necesen"));
+/*import { ThreeElement } from "./folderJs/Uservaliadtion.js";
+console.log(ThreeElement("necesen"));*/
+import { FourElement } from "./folderJs/Uservaliadtion.js";
+console.log(FourElement(["1, 3, 5, 7, 9","1, 10, 5, 15, 9"]));
