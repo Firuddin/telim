c54192698a9373490b3ea36d290e573ac6b72e76
Firuddin <Firuddinaliyev888@gmail.com>
2024-06-13 07:43:18 -0700

    add

---
+++ main.py
@@ -0,0 +1,46 @@
+"""name = input("zehmet olmasa ad daxil edin")
+print("salam"+name)"""
+
+#name = """salam"""
+#print(name)
+
+
+"""student = input("ad daxil et")
+teacher = input("dcsc")
+actor = input("dcjndiu")
+sentence = F'salam,o {student},salam o {teacher}.Salam,O {actor}'
+
+print(sentence)"""
+
+#game = "MainCraft"
+#favorite_drink = "coffee"
+#fruit = "grape"
+
+
+#common = f"""
+#this is favorite {game}
+#this is favorite drink {favorite_drink}
+#I love you {fruit}
+#"""
+#print(common)
+
+"""favoriteFruit = "Apple"
+favoritePrice = 2
+common = "My favoriteFruit is %s.  \
+My favoritePrice is %s..."%(favoriteFruit,favoritePrice)
+print(common)"""
+
+"""name = "ali"
+print(name*2) """
+
+
+
+
+
+name = input("zehmet olmasa ad daxil edin")
+if name == "Admin":
+    print("xos geldin cenab admin")
+elif name == "User":
+    print("xos geldin cenab user")
+else:
+    print("zehmet olmasa duzgun istifadeci daxil edin")
